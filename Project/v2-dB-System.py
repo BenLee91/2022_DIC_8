ca97bfc426b5c1ca28da6219b2d12acb2a00828b
BenLee91 <kkwang3216@gmail.com>
2022-06-24 15:40:00 +0900

    220624_v2

--- Project/v2-dB-System.py
+++ Project/v2-dB-System.py
@@ -4,7 +4,12 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import accuracy_score
+import openpyxl  # excel읽기/저장
+import tkinter
+from tkinter import filedialog
+
 
 # Pyqt5 ui
 from PyQt5 import uic  # PyQt5 내 uic호출
@@ -17,80 +22,9 @@ import plotly.express as px #online
 import sqlite3 as sq
 ##################################################
 # 초기 Setting
-Databasename = 'DIC.db'
+global Databasename, X_data_study, feature_selected
+Databasename = 'test.db'
 form_class=uic.loadUiType('v2-dB_System.ui')[0]  # form_class변수에 ui호출
-
-
-### DB를 라이브러리로 만들어서 호출하는게 더 나을거같은데, 가독성도 떨어지고
-# DB 구성
-def CreateTable():
-    conn = sq.connect(Databasename)  # DB 연결
-    cur = conn.cursor()
-    sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='DIC-feature'" # DB 안에, table 속 결과 가져옴
-    cur.execute(sql)
-    rows = cur.fetchall()
-
-    if not rows:  # DB table 없을 시, 생성
-        sql="CREATE TABLE DIC-feature (idx INTEGER PRIMARY KEY, F1 INTEGER)"
-        cur.execute(sql)
-        conn.commit()
-    conn.close()
-
-def InsertData():
-    f1 = self.X_data_study[0]
-    conn = sq.connect(Databasename)
-    cur = conn.cursor()
-    sql = "INSERT INTO DIC-feature (F1,) VALUES (?,)"
-    cur.execute(sql, (f1,))
-    conn.commit()
-    conn.close()
-    SelectData()  # 데이터 입력 후 DB의 내용 불러와서 TableWidget에 넣기 위한 함수 호출
-
-# def DeleteData(row, column):  # 데이터 삭제는 나중에 update
-#     # 삭제 셀이 눌렸을 때, 삭제 셀은 4번째 셀이므로 column 값이 3일 경우만 작동한다.
-#     if column == 3:
-#         conn = sq.connect(Databasename)
-#         cur = conn.cursor()
-#
-#         # DB의 데이터 idx는 선택한 Row의 첫번째 셀(0번 Column)의 값에 해당한다.
-#         idx = UI_set.tableWidget.item(row, 0).text()
-#
-#         sql = "DELETE FROM aaa WHERE idx =?"
-#         cur.execute(sql, (idx,))
-#         conn.commit()
-#
-#         conn.close()
-#
-#         # 데이터 삭제 후 DB의 내용 불러와서 TableWidget에 넣기 위한 함수 호출
-#         SelectData()
-
-def SelectData():  # DB 데이터 전체 선택
-    conn = sq.connect(Databasename)
-    cur = conn.cursor()
-    sql = "SELECT * FROM DIC-feature"
-    cur.execute(sql)
-    rows = cur.fetchall()
-    conn.close()
-    setTables(rows) # DB의 내용을 불러와서 TableWidget에 넣기 위한 함수 호출
-
-def setTables(row):  # qtable로 데이터 던져주기
-    # DB내부에 저장된 결과물의 갯수를 저장한다.
-    count = len(row)
-    # 갯수만큼 테이블의 Row를 생성한다.
-    UI_set.tableWidget.setRowCount(count)
-
-    # row 리스트만큼 반복하며 Table에 DB 값을 넣는다.
-    for x in range(count):
-        # 리스트 내부의 column쌍은 튜플로 반환하므로 튜플의 각 값을 변수에 저장
-        idx, name, age = row[x]
-
-        # 테이블의 각 셀에 값 입력
-        UI_set.tableWidget.setItem(x, 0, QTableWidgetItem(str(idx)))
-
-def resource_path(relative_path):  #pyinstaller로 압축 시, db 위치전송
-    if hasattr(sys, '_MEIPASS'):
-        return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
 
 class MyWindow(QMainWindow, form_class):  # MyWindow Class로 생성 후, QMainWindow, UI 상속받기
     def __init__(self):
@@ -103,17 +37,19 @@ class MyWindow(QMainWindow, form_class):  # MyWindow Class로 생성 후, QMainW
         self.L3_graph_features.addWidget(self.browser)
 
         # Button Event함수 (self를 적어, ui와 연결)
+        self.L0_btn_Excelfile.clicked.connect(self.L0_btn_Excelfileq)  # Layer0에서 파일 불러오기
         self.L1_btn_OpenFile.clicked.connect(self.L1_btn_OpenFileq)  # Layer1에서 파일 불러오기
         self.L1_btn_preprocessing.clicked.connect(self.L1_btn_preprocessingq)  # Layer1에서 data 전처리
         self.L2_btn_OK.clicked.connect(self.L2_btn_OKq)  # Layer2에서 OK 시, event 연결
         self.L2_btn_NG.clicked.connect(self.L2_btn_NGq)  # Layer2에서 NG 시, event 연결
         self.L3_btn_Analysis.clicked.connect(self.L3_btn_Analysisq)  # Layer3에서 Analysis 진행
 
-        # DB 셋팅
-        self.CreateTable()  #데이터베이스 세팅을 위해 외부 함수 호출
-        self.SelectData()  #데이터베이스 세팅 후, DB 값 불러오기 외부 함수 호출
-
-
+    def L0_btn_Excelfileq(self): #Template 폴더 선택/다운
+        root=tkinter.Tk()
+        root.withdraw()
+        path=filedialog.askdirectory(initialdir="/", title="폴더를 선택해 주세요")#parent=root, initialdir="./", title="폴더를 선택해 주세요")
+        xl=openpyxl.load_workbook('template.xlsx')
+        xl.save(path+'/'+'template.xlsx')
 
     def L1_btn_OpenFileq(self):
         try:
@@ -176,8 +112,9 @@ class MyWindow(QMainWindow, form_class):  # MyWindow Class로 생성 후, QMainW
 
     def L2_btn_OKq(self):
         # Data 학습
-        clf = RandomForestClassifier()  # RandomForest 사용
+        clf = RandomForestRegressor(random_state=531)  # RandomForest 사용
         clf.fit(self.X_train, self.y_train)   # 학습
+        # clf_predict=clf.predict(self.X_test)
         testscore=round(clf.score(self.X_test,self.y_test),3) *100  # 학습 점수
         testscore_label=f'({testscore} %)'  #학습 점수
         self.L3_lbl_TestScore.setText(testscore_label) # 학습 점수
@@ -189,7 +126,7 @@ class MyWindow(QMainWindow, form_class):  # MyWindow Class로 생성 후, QMainW
         ### Xticks update 시키기
         self.browser.setHtml(fig.to_html(include_plotlyjs='cdn'))
 
-        # Table 채우기 - Feature인자
+        # Table 채우기 - Feature인자 + ComboBox 값넣기
         self.L3_tbl_Features.setRowCount(len(self.X_data.columns)) # Feature인자 table row 갯수 정하기
         self.L3_tbl_Features.setColumnCount(1)  # Feature인자 table column 갯수 정하기
         self.L3_tbl_Features.setHorizontalHeaderLabels(['Feature 상관도 순위'])
@@ -197,18 +134,73 @@ class MyWindow(QMainWindow, form_class):  # MyWindow Class로 생성 후, QMainW
             value = self.X_data.columns[self.indices][row_index]
             item = QTableWidgetItem(str(value))
             self.L3_tbl_Features.setItem(row_index, 0, item)
+        feature_count = range(1,len(self.X_data.columns)+1) #comboBox 갯수 나열
+        combo_count=list(map(str, list(feature_count))) #comboBox list화
+        self.L3_cb_comboBox.addItems(combo_count) #comboBox list반영
 
     def L2_btn_NGq(self):
         print('NG')
 
     def L3_btn_Analysisq(self):
         # 상관성 높은 데이터로 학습 데이터 축소
+        global X_data_study, feature_selected
         combo_selected= str(self.L3_cb_comboBox.currentText())
         feature_selected=[]
+
         for i in range(int(combo_selected)):
             feature_selected.append(self.L3_tbl_Features.item(i, 0).text())
-        self.X_data_study=self.X_data[feature_selected]
-        print(self.X_data_study)
+        X_data_study=self.X_data[feature_selected]
+
+        # DB 셋팅
+        CreateTable()  #데이터베이스 생성
+
+# DB 구성
+def CreateTable():
+    conn = sq.connect(Databasename)  # DB 연결
+    # conn.execute("DELETE FROM users").rowcount
+    cur = conn.cursor()
+    sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='feature'" # DB 안에, table 속 결과 가져옴
+    cur.execute(sql)
+    rows = cur.fetchall()
+    if not rows:  # DB table 없을 시, 생성
+        sql="CREATE TABLE feature (F1 INTEGER, F2 INTEGER, F3 INTEGER, F4 INTEGER)"#F1 FLOAT, F2 FLOAT, F3 FLOAT, F4 FLOAT)"  idx INTEGER PRIMARY KEY,
+        cur.execute(sql)
+        conn.commit()
+    conn.close()
+    InsertData()
+
+def InsertData():
+    conn = sq.connect(Databasename)
+    cur = conn.cursor()
+    rows = cur.fetchall()
+    if not rows:  # DB table 있을 시, 생성
+        # sql = "TRUNCATE TABLE feature" # DB초기화 but sqlite에서는 TRUNCATE 명령어 지원안함.
+        sql= "DELETE FROM feature"
+        cur.execute(sql)
+        conn.commit()
+    for row in X_data_study.itertuples():
+        sql = "insert into feature(F1, F2, F3, F4) values(?,?,?,?)"
+        # cur.execute(sql, (X_data_study.iat[0, 0], X_data_study.iat[0, 1], X_data_study.iat[0, 2], X_data_study.iat[0, 3]))
+        cur.execute(sql, (row[1], row[2], row[3], row[4]))
+        print(row)
+    conn.commit()
+    conn.close()
+    print(5)
+    # SelectData()  # 데이터 입력 후 DB의 내용 불러와서 TableWidget에 넣기 위한 함수 호출
+
+# def SelectData():  # DB 데이터 전체 선택
+#     conn = sq.connect(Databasename)
+#     cur = conn.cursor()
+#     sql = "SELECT * FROM DIC-feature"
+#     cur.execute(sql)
+#     rows = cur.fetchall()
+#     conn.close()
+#     Real_Train_Data(rows) # DB의 내용을 불러와서 TableWidget에 넣기 위한 함수 호출
+#
+# def resource_path(relative_path):  #pyinstaller로 압축 시, db 위치전송
+#     if hasattr(sys, '_MEIPASS'):
+#         return os.path.join(sys._MEIPASS, relative_path)
+#     return os.path.join(os.path.abspath("."), relative_path)
 
 if __name__=="__main__":  # 프로그램 실행
     app=QApplication(sys.argv)  # QApplication 객체생성
